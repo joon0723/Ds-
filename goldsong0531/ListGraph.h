e0bd2b81a258b48de115c6982eab9f9ef5a77a2f
joon0723 <86287055+joon0723@users.noreply.github.com>
2022-11-28 22:50:48 +0900

    Add files via upload

---
+++ goldsong0531/ListGraph.h
@@ -0,0 +1,20 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include "Graph.h"
+
+class ListGraph : public Graph{
+private:
+	map < int, int >* m_List, * m_List_nodir;
+
+public:	
+	ListGraph(bool type, int size);
+	~ListGraph();
+		
+	void getAdjacentEdges(int vertex, map<int, int>* m);
+	void getAdjacentEdgesWithoutDir(int vertex, map<int, int>* m);
+	void insertEdge(int from, int to, int weight);
+	bool printGraph();
+};
+
+#endif
